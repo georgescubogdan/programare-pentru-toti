1c3196ca5cdfc1a059d5e61ca2e740da7179b9d5
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-07 13:18:45 +0300

    Create Problema 2.c

---
+++ Lectia 1/Probleme medii/Problema 2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i; 
+	double temp, rezultat;
+	char operatie;
+	
+	printf("Dati numarul n: ");
+	scanf("%d", &n);
+	
+	printf("Introduceti operatia: ");
+	scanf(" %c", &operatie);
+	
+	printf("Dati primul numar: ");
+	scanf("%lf", &rezultat);
+	
+	if (operatie == '+')
+	{
+		for (i = 0; i < n-1; i++)
+		{
+			printf("Dati inca un numar: ");
+			scanf("%lf", &temp);
+			rezultat += temp;
+		}
+	}
+	else if (operatie == '-')
+	{
+		for (i = 0; i < n-1; i++)
+		{
+			printf("Dati inca un numar: ");
+			scanf("%lf", &temp);
+			rezultat -= temp;
+		}
+	}	
+	else if (operatie == '*')
+	{
+		for (i = 0; i < n-1; i++)
+		{
+			printf("Dati inca un numar: ");
+			scanf("%lf", &temp);
+			rezultat *= temp;
+		}
+	}
+	else if (operatie == '/')
+	{
+		for (i = 0; i < n-1; i++)
+		{
+			printf("Dati inca un numar: ");
+			scanf("%lf", &temp);
+			rezultat /= temp;
+		}
+	}
+	
+	printf("Rezultatul final este: %lf\n", rezultat);
+	
+	return 0;
+}
