51a876bc472d5a18d7cbe9191737bf5031c4da8a
Georgescu Bogdan <georgescubogdan96@gmail.com>
2019-10-07 13:30:36 +0300

    Update Problema 2.c
    reindent

--- Lectia 1/Probleme usoare/Problema 2.c
+++ Lectia 1/Probleme usoare/Problema 2.c
@@ -2,19 +2,19 @@
 
 int main() 
 {
-	int nr1, nr2, nr3, suma = 0;
+    int nr1, nr2, nr3, suma = 0;
 	
-	printf("Primul numar: ");
-	scanf("%d", &nr1);
+    printf("Primul numar: ");
+    scanf("%d", &nr1);
+
+    printf("Al doilea numar: ");
+    scanf("%d", &nr2);
+
+    printf("Al treilea numar: ");
+    scanf("%d", &nr3);
 	
-	printf("Al doilea numar: ");
-	scanf("%d", &nr2);
-	
-	printf("Al treilea numar: ");
-	scanf("%d", &nr3);
-	
-	suma = nr1 + nr2 + nr3;
-	printf("Suma numerelor este: %d\n", suma);
+    suma = nr1 + nr2 + nr3;
+    printf("Suma numerelor este: %d\n", suma);
 	
     return 0;
 }
