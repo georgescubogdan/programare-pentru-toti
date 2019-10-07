61cb0842679c34976f27ce0f333aa854b3366161
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-07 12:35:29 +0300

    Create Problema 3.c

---
+++ Lectia 1/Probleme usoare/Problema 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+     
+int main()
+{
+	int numar, cifra, suma = 0;
+     
+    printf("Introduceti un numar: ");
+    scanf("%d", &numar);
+         
+    while (numar != 0)
+    {
+        cifra = numar % 10;
+        suma += cifra;
+        numar = numar / 10;
+    }
+     
+    printf("Suma cifrelor este: %d\n", suma);
+    
+    return 0;
+ }
