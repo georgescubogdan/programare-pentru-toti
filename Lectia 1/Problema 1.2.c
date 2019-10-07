59e5f318a6925767c6dc3851b3d5b1d87ff89403
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-07 12:02:23 +0300

    Create Problema 1.2.c

---
+++ Lectia 1/Problema 1.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+	int nr1, nr2, nr3, suma = 0;
+	
+	printf("Primul numar: ");
+	scanf("%d", &nr1);
+	
+	printf("Al doilea numar: ");
+	scanf("%d", &nr2);
+	
+	printf("Al treilea numar: ");
+	scanf("%d", &nr3);
+	
+	suma = nr1 + nr2 + nr3;
+	printf("Suma numerelor este: %d\n", suma);
+	
+    return 0;
+}
