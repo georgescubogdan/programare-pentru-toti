aa394f03e9f93610366ec620371288e85f223aa0
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-14 10:15:01 +0300

    Create Problema1.c

---
+++ Lectia 2/Probleme usoare/Problema1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, v[20];
+
+	for (i = 0; i < 20; i++){
+		v[i] = i;
+	}
+
+	printf("Elementele vectorului sunt: ");
+	
+	for (i = 0; i < 20; i++){
+		printf("%d ", v[i]);
+	
+	}
+	printf("\n");
+
+	return 0;
+}
