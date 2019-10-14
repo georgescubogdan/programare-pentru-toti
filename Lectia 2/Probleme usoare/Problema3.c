c9c5736f9d90bd9d16b7b15a5a217a0fc007325b
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-14 10:18:09 +0300

    Create Problema3.c

---
+++ Lectia 2/Probleme usoare/Problema3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j, suma = 0, m[10][10];
+
+//Initializare
+	for (i = 0; i < 10; i++){
+		for (j = 0; j < 10; j++){
+			m[i][j] = i + j;
+		}
+	}
+
+//Afisare
+	for (i = 0; i < 10; i++){
+		for (j = 0; j < 10; j++){
+			printf("%d ", m[i][j]);
+		}
+		printf("\n");
+	}
+
+//Calcul suma
+	for (i = 0; i < 10; i++){
+		for (j = 0; j < 10; j++){
+			if (i == j){
+				suma += m[i][j];
+			}
+		}
+	}
+
+	printf("Suma elementelor de pe diagonala principala este: %d\n", suma);
+
+	return 0;
+}
