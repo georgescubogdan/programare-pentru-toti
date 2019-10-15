d4c4c4c4ffb5fe17ca52160c78328b2599687981
Georgescu Bogdan <georgescubogdan96@gmail.com>
2019-10-15 10:02:26 +0300

    Create Problema 1.c

---
+++ Lectia 2/Probleme Grele/Problema 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  int i, n, v[100];
+  scanf("%d", &n);
+  
+  for (i = 0; i < 2 * n - 1; i++) {
+    scanf("%d", &v[i]);
+  }
+  
+  int numar_cautat = 0;
+  
+  for (i = 0; i < 2 * n - 1; i++) {
+    numar_cautat = numar_cautat ^ v[i];
+  }
+  
+  printf("Numarul cautat este: %d\n", numar_cautat);
+  
+  return 0;
+}
