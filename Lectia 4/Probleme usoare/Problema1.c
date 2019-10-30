5eca1d60b2c44c1095bf908687c26bc9e7331adb
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-30 22:14:26 +0200

    Update Problema1.c

--- Lectia 4/Probleme usoare/Problema1.c
+++ Lectia 4/Probleme usoare/Problema1.c
@@ -1,1 +1,31 @@
+/**
+ * Authors: PPT Team
+ **/
 
+#include <stdio.h>
+
+int square(int number)
+{
+    int aux;
+    aux = number * number;
+    return aux;
+    
+    /*
+    Sau 
+    return number * number;
+    */
+}
+
+int main()
+{
+    int n, temp;
+    printf("Introduceti un numar: ");
+    scanf("%d", &n);
+    temp = square(n);
+    /*
+    Sau
+    printf("Patratul numarului este: %d\n", square(n));
+    */
+    printf("Patratul numarului este: %d\n", temp);
+    return 0 ;
+} 
