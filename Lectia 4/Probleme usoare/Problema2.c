87f9211628cae8fc69c6b98bbf62db70b13cbad4
razvandru <33982595+razvandru@users.noreply.github.com>
2019-10-30 22:27:47 +0200

    Update Problema2.c

--- Lectia 4/Probleme usoare/Problema2.c
+++ Lectia 4/Probleme usoare/Problema2.c
@@ -1,1 +1,32 @@
+/**
+ * Authors: PPT Team
+ **/
 
+#include <stdio.h>
+
+int checkPrime(int number)
+{   
+    int i;
+    for (i = 2; i < number/2; i++) {
+        if (number % i == 0)
+            return 0;
+    }
+    return 1;
+
+}
+
+int main()
+{
+    int n, temp;
+    printf("Introduceti un numar: ");
+    scanf("%d", &n);
+    temp = checkPrime(n);
+
+    if (temp == 0) {
+        printf("Numarul nu este prim!\n");
+    } else {
+        printf("Numarul este prim!\n");
+    }
+    
+    return 0 ;
+} 
