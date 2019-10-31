cdbff4fece2c06a5822c03bf210f3d8e8a2c5444
Bogdan Georgescu <georgescubogdan96@gmail.com>
2019-10-31 11:38:18 +0200

    small changes

--- Lectia 4/Probleme medii/Problema2.c
+++ Lectia 4/Probleme medii/Problema2.c
@@ -6,10 +6,11 @@
 
 int cmmdc(int n1, int n2)
 {
-    if (n2 != 0)
+    if (n2 != 0) {
        return cmmdc(n2, n1%n2);
-    else 
+    } else {
        return n1;
+    }
 }
 
 int main()
--- Lectia 4/Probleme usoare/Problema2.c
+++ Lectia 4/Probleme usoare/Problema2.c
@@ -8,8 +8,9 @@ int checkPrime(int number)
 {   
     int i;
     for (i = 2; i < number/2; i++) {
-        if (number % i == 0)
+        if (number % i == 0) {
             return 0;
+        }
     }
     return 1;
 
