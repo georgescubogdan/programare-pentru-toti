5266eb6853cbcb1751caa5dcfd97b8fc8b4d87b1
razvandru <33982595+razvandru@users.noreply.github.com>
2019-11-07 19:29:49 +0200

    Create Problema3.c

---
+++ Lectia 5/Probleme usoare/Problema3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int* v;
+    int n, i;
+    printf("Dati lungimea vectorului: ");
+    scanf("%d", &n);
+
+    v = (int*)calloc(n, sizeof(int));
+    for (i = 0; i <= n; i++) {
+        v[i] = i;
+        printf("v[%d]: %d\n", i, v[i]);
+    }
+    return 0;
+}
